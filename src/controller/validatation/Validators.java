a881104b2bf3a5db15cff7e39b6c1598475bf9dc
Vasiliki <mpoutavasiliki@gmail.com>
2022-05-10 23:14:00 +0300

    phone number length

--- src/controller/validatation/Validators.java
+++ src/controller/validatation/Validators.java
@@ -2,19 +2,20 @@ package controller.validatation;
 
 public class Validators {
 
+     static final int VALID_PHONE_NUMBER_LENGTH = 8;
 
     public static boolean validateNotEmpty(String string) {
         return !string.isBlank();
     }
-
 
     public static boolean validateEmail(String email) {
         return validateNotEmpty(email) && email.contains("@");
     }
 
     public static boolean validatePhoneNumber(String phoneNumber) {
-        return validateNotEmpty(phoneNumber) && phoneNumber.chars().allMatch(Character::isDigit);
+        return validateNotEmpty(phoneNumber)
+                && phoneNumber.length() >= VALID_PHONE_NUMBER_LENGTH
+                && phoneNumber.chars().allMatch(Character::isDigit);
     }
-
 }
 
